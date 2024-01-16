987e77467c168b15a005e1e34c697f7587999976
armankadri121 <147044189+armankadri121@users.noreply.github.com>
2024-01-16 07:55:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var t1= gsap.timeline();
+
+t1.to("#loader",{
+    top: "-100vh",
+    delay: 4,
+    duration: 2,
+})
+
+t1.from("#navbar",{
+    y: -200,
+    duration: 1,
+    delay: 1,
+})
+
+t1.from("#part1, #part2, #part3",{
+    y: -100,
+    duration: 1,
+    opacity: 0,
+    stagger: 0.5,
+})
+
+t1.from("#main h1",{
+    x: -2000,
+    duration: 2,
+    stagger: 0.5,
+})
+
+t1.from("#main img",{
+    x:100,
+    opacity: 0,
+})
